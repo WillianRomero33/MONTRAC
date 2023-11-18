2de615ed8e8b91d1294a5d0da9f4e8509329a949
Javier <javierivera29@gmail.com>
2023-11-18 01:06:26 -0600

    Solucion de error de welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.app', ['pageSlug' => 'dashboard'])
 
+@auth
+<script type="text/javascript">
+    window.location = "{{ url('/home') }}";//here double curly bracket
+</script>
+@else
 @section('content')
-    <div class="header py-8 py-lg-10">
-        <div class="container">
-            <div class="header-body text-center mb-7">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 col-md-8">
-                        <img class="" alt="" src="{{asset('black/img/bienvenidos.png')}}"  style=" width: 750px; height: 100px;"/>
-                        <p class="text-lead " style="font-size: 60px;">
-                            {{ __('Monitor de Medios de Transporte') }}
-                        </p>
-                    </div>
-                    <img class="w-100" alt="" src="{{asset('black/img/zf-pacifico.jpg')}}" style="background-size:cover"/>
+<div class="header py-8 py-lg-10">
+    <div class="container">
+        <div class="header-body text-center mb-7">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-8">
+                    <img class="" alt="" src="{{asset('black/img/bienvenidos.png')}}"  style=" width: 750px; height: 100px;"/>
+                    <p class="text-lead " style="font-size: 60px;">
+                        {{ __('Monitor de Medios de Transporte') }}
+                    </p>
                 </div>
+                <img class="w-100" alt="" src="{{asset('black/img/zf-pacifico.jpg')}}" style="background-size:cover"/>
             </div>
         </div>
     </div>
+</div>
 @endsection
+@endauth
